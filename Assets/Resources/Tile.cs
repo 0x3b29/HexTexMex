7d88a71ab7ffa6089fe5bab64df3b8dd4490143d
oli4@home <wagener_olivier@hotmail.com>
2019-10-06 20:40:42 +0200

    save initial work after brainstorming

---
+++ Assets/Resources/Tile.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject tileGameObject;
+    public Tile leftTile;
+    public Tile rightTile;
+    public Tile topLeftTile;
+    public Tile topRightTile;
+    public Tile lowerLeftTile;
+    public Tile lowerRightTile;
+
+    public int xCoordinate;
+    public int yCoordinate;
+
+    public bool isWater;
+    public bool isForest;
+    public bool isWheat;
+    public bool isWell;
+    public bool isRock;
+
+    public void setInitialValues(int xCoordinate, int yCoordinate, GameObject tileGameObject)
+    {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+
+        this.tileGameObject = tileGameObject;
+    }
+
+    public List<Tile> getNeigbhours()
+    {
+        List <Tile> neighbourList = new List<Tile>();
+
+        if (leftTile != null) neighbourList.Add(leftTile);
+        if (rightTile != null) neighbourList.Add(rightTile);
+        if (topLeftTile != null) neighbourList.Add(topLeftTile);
+        if (topRightTile != null) neighbourList.Add(topRightTile);
+        if (lowerLeftTile != null) neighbourList.Add(lowerLeftTile);
+        if (lowerRightTile != null) neighbourList.Add(lowerRightTile);
+
+        return neighbourList;
+    }
+
+    public int neighboursWaterCount()
+    {
+        int neighboursWaterCount = 0;
+
+        foreach (Tile tile in getNeigbhours())
+        {
+            if (tile.isWater) neighboursWaterCount++;
+        }
+
+        return neighboursWaterCount;
+    }
+
+    public int neighboursTreeCount()
+    {
+        int neighboursTreeCount = 0;
+
+        foreach (Tile tile in getNeigbhours())
+        {
+            if (tile.isForest) neighboursTreeCount++;
+        }
+
+        return neighboursTreeCount;
+    }
+
+    public int neighboursRockCount()
+    {
+        int neighboursRockCount = 0;
+
+        foreach (Tile tile in getNeigbhours())
+        {
+            if (tile.isRock) neighboursRockCount++;
+        }
+
+        return neighboursRockCount;
+    }
+}
---
+++ Assets/SpawnTiles.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTiles : MonoBehaviour
+{
+    private const int x = 60;
+    private const int y = 60;
+
+    private const float newWaterProbability = 0.007f;
+    private const float adjacentWaterProbability = 0.35f;
+
+    private const float newTreeProbability = 0.015f;
+    private const float adjacentTreeProbability = 0.20f;
+
+    private const float wellProbability = 0.0025f;
+    private const float wheatProbability = 0.008f;
+
+    private const float newRockProbability = 0.010f;
+    private const float adjacentRockProbability = 0.20f;
+
+    private Tile[] tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles = new Tile[x * y];
+
+        Material water = Resources.Load("Water", typeof(Material)) as Material;
+        Material earth = Resources.Load("Earth", typeof(Material)) as Material;
+        Material stone = Resources.Load("Stone", typeof(Material)) as Material;
+
+        Material[] waterTile = new Material[2];
+        waterTile[0] = water;
+        waterTile[1] = water;
+
+        Material[] earthTile = new Material[2];
+        earthTile[0] = earth;
+        earthTile[1] = earth;
+
+        Material[] stoneTile = new Material[2];
+        stoneTile[0] = stone;
+        stoneTile[1] = stone;
+
+        int tileCounter = 0;
+
+        // Create Tiles 
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                GameObject newTile;
+
+                newTile = Instantiate(Resources.Load("Hex Parent") as GameObject, new Vector3(j * 1.7f + (i % 2 * 0.85f), 0, i * 1.5f), Quaternion.identity);
+                newTile.name = "Tile" + i + "-" + j;
+                
+                int grassKind = Mathf.RoundToInt(Random.Range(1f, 3f));
+                Material[] tileMats = newTile.transform.Find("Hexagon").GetComponent<Renderer>().materials;
+                tileMats[1] = Resources.Load("Grass" + grassKind, typeof(Material)) as Material;
+                newTile.transform.Find("Hexagon").GetComponent<Renderer>().materials = tileMats;
+
+                // Attach tile script
+                tiles[tileCounter] = newTile.AddComponent<Tile>(); 
+                tiles[tileCounter].setInitialValues(i, j, newTile);
+
+                tileCounter++;
+            }
+        }
+
+        // Connect tiles 
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                GameObject goThisTile = GameObject.Find("Tile" + i + "-" + j);
+
+                GameObject goLeftTile = GameObject.Find("Tile" + i + "-" + (j - 1));
+                GameObject goRightTile = GameObject.Find("Tile" + i + "-" + (j + 1));
+
+                GameObject goTopLeftTile = GameObject.Find("Tile" + (i + 1) + "-" + ((j - 1) + (i % 2)));
+                GameObject goTopRightTile = GameObject.Find("Tile" + (i + 1) + "-" + (j + (i % 2)));
+
+                GameObject goLowerLeftTile = GameObject.Find("Tile" + (i - 1) + "-" + ((j - 1) + (i % 2)));
+                GameObject goLowerRightTile = GameObject.Find("Tile" + (i - 1) + "-" + (j + (i % 2)));
+
+                Tile tile = goThisTile.GetComponent<Tile>();
+                
+                if (goLeftTile) tile.leftTile = goLeftTile.GetComponent<Tile>();
+                if (goRightTile) tile.rightTile = goRightTile.GetComponent<Tile>();
+                if (goTopLeftTile) tile.topLeftTile = goTopLeftTile.GetComponent<Tile>();
+                if (goTopRightTile) tile.topRightTile = goTopRightTile.GetComponent<Tile>();
+                if (goLowerLeftTile) tile.lowerLeftTile = goLowerLeftTile.GetComponent<Tile>();
+                if (goLowerRightTile) tile.lowerRightTile = goLowerRightTile.GetComponent<Tile>();
+            }
+        }
+
+        // Set Water
+        for(int i = 0; i < x * y; i++)
+        {
+            Tile tile = tiles[i];
+
+            float waterProbability = newWaterProbability + (adjacentWaterProbability * tile.neighboursWaterCount());
+
+            if (Random.Range(0f, 1f) < waterProbability)
+            {
+                tile.tileGameObject.transform.Find("Hexagon").GetComponent<Renderer>().materials = waterTile;
+                tile.isWater = true;
+            }
+        }
+
+        // Set Trees
+        for (int i = 0; i < x * y; i++)
+        {
+            Tile tile = tiles[i];
+
+            float treeProbability = newTreeProbability + (adjacentTreeProbability * tile.neighboursTreeCount());
+
+            if (!tile.isWater && Random.Range(0f, 1f) < treeProbability)
+            {
+                tile.tileGameObject.transform.Find("Hexagon").GetComponent<Renderer>().materials = earthTile;
+                tile.isForest = true;
+
+                GameObject newTree;
+                string treeToSpawn;
+                int neighboursTreeCount = tile.neighboursTreeCount();
+
+                if (neighboursTreeCount == 0)
+                {
+                    treeToSpawn = "OneTreeParent";
+                }
+                else if (neighboursTreeCount == 1)
+                {
+                    treeToSpawn = "TwoTreeParent";
+                }
+                else
+                {
+                    treeToSpawn = "ThreeTreeParent";
+                }
+
+                newTree = Instantiate(Resources.Load(treeToSpawn) as GameObject, tile.tileGameObject.transform.position, Quaternion.identity); //Vector3(j * 1.7f + (i % 2 * 0.85f), 0, i * 1.5f)
+                newTree.transform.parent = tile.tileGameObject.transform;
+                newTree.name = "Tree";
+            }
+        }
+
+        // Set Wells
+        for (int i = 0; i < x * y; i++)
+        {
+            Tile tile = tiles[i];
+
+            if (!tile.isWater && !tile.isForest && Random.Range(0f, 1f) < wellProbability)
+            {
+                GameObject well;
+                well = Instantiate(Resources.Load("Well") as GameObject, tile.tileGameObject.transform.position, Quaternion.identity);
+                well.transform.parent = tile.tileGameObject.transform;
+                well.name = "Well";
+            }
+        }
+
+        // Set Wheat
+        for (int i = 0; i < x * y; i++)
+        {
+            Tile tile = tiles[i];
+
+            if (!tile.isWater && !tile.isForest && !tile.isWell && Random.Range(0f, 1f) < wheatProbability)
+            {
+                tile.tileGameObject.transform.Find("Hexagon").GetComponent<Renderer>().materials = earthTile;
+                tile.isWheat = true;
+
+                GameObject WheatParent;
+                WheatParent = Instantiate(Resources.Load("WheatParent") as GameObject, tile.tileGameObject.transform.position, Quaternion.identity);
+                WheatParent.transform.parent = tile.tileGameObject.transform;
+                WheatParent.name = "WheatParent";
+            }
+        }
+
+        // Set Rocks
+        for (int i = 0; i < x * y; i++)
+        {
+            Tile tile = tiles[i];
+            float RockProbability = newRockProbability + (adjacentRockProbability * tile.neighboursRockCount());
+
+            if (!tile.isWater && !tile.isForest && !tile.isWell && !tile.isWheat && Random.Range(0f, 1f) < RockProbability)
+            {
+                tile.tileGameObject.transform.Find("Hexagon").GetComponent<Renderer>().materials = stoneTile;
+                tile.isRock = true;
+                
+                GameObject Rock;
+                string rockToSpawn;
+                int neighboursRockCount = tile.neighboursRockCount();
+
+                if (neighboursRockCount == 0)
+                {
+                    rockToSpawn = "OneStone";
+                }
+                else if (neighboursRockCount == 1)
+                {
+                    rockToSpawn = "TwoStones";
+                }
+                else if (neighboursRockCount == 2)
+                {
+                    rockToSpawn = "ThreeStones";
+                }
+                else
+                {
+                    rockToSpawn = "FourStones";
+                }
+
+                Rock = Instantiate(Resources.Load(rockToSpawn) as GameObject, tile.tileGameObject.transform.position, Quaternion.identity);
+                Rock.transform.parent = tile.tileGameObject.transform;
+                Rock.name = "Rock";
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
