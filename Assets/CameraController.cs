2921efa0de1ce27f00b5be3e0435eca96cf7653b
oli4@home <wagener_olivier@hotmail.com>
2019-10-08 00:57:21 +0200

    add a script to move the camera around

---
+++ Assets/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Inspired by https://gist.github.com/JohannesMP/e15fe61386d4381d4441c3c324d96c56
+public class CameraController : MonoBehaviour {
+      
+    float speed = 25.0f;
+    float mouseSensitivity = 0.2f;
+    private Vector3 oldMousePosition; 
+     
+    void Update ()
+    {
+        // Reset position on mouse btn down to prevent jumps
+        if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            oldMousePosition = Input.mousePosition;
+        }
+
+        // Mouse movement
+        if (Input.GetKey(KeyCode.Mouse2))
+        {
+            // Calculate delta
+            Vector3 mouseDelta = Input.mousePosition - oldMousePosition;
+            oldMousePosition = Input.mousePosition;
+
+            // Multiply with sensitivity
+            float xMovement = -mouseDelta.y * mouseSensitivity;
+            float yMovement = mouseDelta.x * mouseSensitivity;
+
+            // Add movement 
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x + xMovement, transform.eulerAngles.y + yMovement, 0);
+        }
+
+        // Keyboard movement
+        Vector3 keyboardMovement = new Vector3();
+
+        if (Input.GetKey(KeyCode.W)) keyboardMovement += new Vector3(0, 0, 1);
+        if (Input.GetKey(KeyCode.A)) keyboardMovement += new Vector3(-1, 0, 0);
+        if (Input.GetKey(KeyCode.S)) keyboardMovement += new Vector3(0, 0, -1);        
+        if (Input.GetKey(KeyCode.D)) keyboardMovement += new Vector3(1, 0, 0);
+
+        transform.Translate(keyboardMovement * speed * Time.deltaTime);      
+    }
+}
