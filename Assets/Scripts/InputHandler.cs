9b48d75f8e1954022d393977481569806bfe2ec2
oli4@home <wagener_olivier@hotmail.com>
2019-10-13 22:59:30 +0200

    feature: add a hut when clicking on a tile

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Debug.Log("Mouse down");
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+
+                GameObject woodHouse = Instantiate(Resources.Load("Woodhouse Parent") as GameObject, hit.transform.position, Quaternion.identity);
+                woodHouse.transform.parent = hit.transform.gameObject.transform;
+                woodHouse.name = "woodHouse";
+
+            }
+        }
+    }
+}
