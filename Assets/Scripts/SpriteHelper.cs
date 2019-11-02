e9080c7519ad723612588ad02e1cb74aa685f0ad
oli4@home <wagener_olivier@hotmail.com>
2019-11-02 15:26:53 +0100

    Sprites: Add a new scene with a setup to generate sprites

---
+++ Assets/Scripts/SpriteHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteHelper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Capture a screenshot with P
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ScreenCapture.CaptureScreenshot("Screenshots/" + System.DateTime.Now.ToString("dd-MM-yy_hh-mm-ss") + ".png");
+            Debug.Log("Captured : " + "Screenshots / " + System.DateTime.Now.ToString("dd - MM - yy_hh - mm - ss") + ".png");
+        }
+    }
+}
