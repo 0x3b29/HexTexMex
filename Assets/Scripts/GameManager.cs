1e265907252b92dae8ffa92cd8cacd6ebc8198b4
oli4@home <wagener_olivier@hotmail.com>
2019-11-02 11:45:00 +0100

    GameManager: change third players color to make him more visible

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour
         turnManager = GetComponent<TurnManager>();
         turnManager.AddPlayer(new Player("Jérôme", Color.red, 10, 10, 10));
         turnManager.AddPlayer(new Player("Olivier", Color.blue, 10, 10, 10));
-        turnManager.AddPlayer(new Player("Gérard", Color.green, 10, 10, 10));
+        turnManager.AddPlayer(new Player("Gérard", Color.yellow, 10, 10, 10));
         turnManager.EndTurn();
 
         // Create Map to play on
