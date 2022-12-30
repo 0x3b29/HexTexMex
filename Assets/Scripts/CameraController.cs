33279c78f067fa8eb3c288272db3958c83c13388
Olivier Wagener <wagener_olivier@hotmail.com>
2022-12-30 19:40:32 +0100

    feat: rewrite camera movement to make it feel more snappy and not lerp positions during rotation but only rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,45 +4,72 @@ using UnityEngine;
 // Inspired by https://gist.github.com/JohannesMP/e15fe61386d4381d4441c3c324d96c56
 public class CameraController : MonoBehaviour {
       
-    float speed = 25.0f;
-    float mouseSensitivity = 0.2f;
+    public float speed = 25.0f;
+    public float mouseSensitivity = 0.2f;
 
-    float cameraRotationLerpSpeed = 3f;
-    float cameraPositionLerpSpeed = 3f;
+    public float cameraRotationLerpSpeed = 3f;
+    public float cameraPositionLerpSpeed = 3f;
 
-    private int minZoomLevel = 3;
-    private int maxZoomLevel = 10;
-    private int currentZoomLevel = 8;
-    private float cameraPanFactor = 2f;
+    public int minZoomSteps = 3;
+    public int maxZoomSteps = 10;
+    public int currentStep = 8;
+
+    public float zoomTiltStep = 2f;
+    public float zoomOffsetYStep = 5f;
+    public float zoomOffsetZStep = -10f;
 
     private Vector3 oldMousePosition;
-    private GameObject cameraContainer;
-    private GameObject mainCamera;
-    private GameObject cameraHitPoint;
 
-    private GameObject cameraContainerTarget;
-    private GameObject mainCameraTarget;
+    public GameObject cameraCenter;
+    public GameObject cameraOffset;
+    public GameObject mainCamera;
+
+    public GameObject cameraCenterTarget;
+    public GameObject cameraOffsetTarget;
+    public GameObject mainCameraTarget;
+
+    public GameObject cameraHitPoint;
+
+    private Vector3 currentPosition;
+    private Vector3 targetPosition;
+
+    private Quaternion currentRotation;
+    private Quaternion targetRotation;
+
+    private Vector3 targetOffset;
+    private Vector3 currentOffset;
+
+    private Quaternion currentTilt;
+    private Quaternion targetTilt;
 
     private void Awake()
     {
-        cameraHitPoint = GameObject.Find("Camera Hit Point");
+        currentRotation = cameraCenter.transform.rotation;
+        targetRotation = cameraCenter.transform.rotation;
 
-        cameraContainer = GameObject.Find("CameraContainer");
-        mainCamera = GameObject.Find("Main Camera");
-
-        cameraContainerTarget = GameObject.Find("Camera Container Target");
-        mainCameraTarget = GameObject.Find("Main Camera Target");
+        currentPosition = cameraCenter.transform.position;
+        targetPosition = cameraCenter.transform.position;
     }
 
     void Update ()
     {
         // Set a marker to the point in space where the camera intersects with the world floor
         RaycastHit hit;
-        if (!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.E) && !Input.GetKey(KeyCode.Mouse2) && 
-            Physics.Raycast(mainCameraTarget.transform.position, mainCameraTarget.transform.forward, out hit, 100, 1 << Constants.worldFloorLayer))
+        Vector3 startPosition = cameraCenterTarget.transform.position;
+        startPosition.y = 100;
+
+        // Test the height of the center.
+        if (Physics.Raycast(
+                startPosition,
+                Vector3.down,
+                out hit,
+                1000,
+                LayerMask.GetMask(
+                    LayerMask.LayerToName(Constants.worldFloorLayer),
+                    LayerMask.LayerToName(Constants.tileLayer))))
         {
-            Vector3 newPosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-            cameraHitPoint.transform.position = newPosition;
+            cameraHitPoint.transform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            targetPosition.y = hit.point.y;
         }
 
         // Reset position on mouse btn down to prevent jumps
@@ -51,7 +78,7 @@ public class CameraController : MonoBehaviour {
             oldMousePosition = Input.mousePosition;
         }
         
-        // Mouse movement
+        // Calculate camera rotation with middle mouse button
         if (Input.GetKey(KeyCode.Mouse2))
         {
             // Calculate delta
@@ -62,92 +89,123 @@ public class CameraController : MonoBehaviour {
             float yMovement = mouseDelta.x * mouseSensitivity;
 
             // Set to new rotation
-            cameraContainerTarget.transform.RotateAround(cameraHitPoint.transform.position, Vector3.up, yMovement);
+            targetRotation = targetRotation *= Quaternion.Euler(0, yMovement, 0);
+        }
+
+        // Select the current zoomlevel based on scroll
+        if (Input.mouseScrollDelta.y > 0.0f && currentStep > minZoomSteps)
+        {
+            currentStep -= 1;
+        }
+        else if (Input.mouseScrollDelta.y < 0.0f && currentStep < maxZoomSteps)
+        {
+            currentStep += 1;
         }
 
         // Keyboard movement of the camera container
-        Vector3 cameraContainerMovement = new Vector3();
+        Vector3 cameraTranslation = new Vector3();
 
-        // Zoom in with the scroll wheel
-        if (Input.mouseScrollDelta.y > 0.0f && currentZoomLevel > minZoomLevel)
-        {
-            currentZoomLevel -= 1;
-            cameraContainerMovement += new Vector3(0, -5, +10);
-
-            // Make camera angle more shallow
-            Vector3 mainCameraRotation = mainCameraTarget.transform.rotation.eulerAngles;
-            mainCameraRotation.x -= cameraPanFactor;
-            mainCameraTarget.transform.rotation = Quaternion.Euler(mainCameraRotation);
-        }
-
-        // Zoom out with the scroll wheel
-        if (Input.mouseScrollDelta.y < 0.0f && currentZoomLevel < maxZoomLevel)
-        {
-            currentZoomLevel += 1;
-            cameraContainerMovement += new Vector3(0, +5, -10);
-
-            // Make camera angle more steep
-            Vector3 mainCameraRotation = mainCameraTarget.transform.rotation.eulerAngles;
-            mainCameraRotation.x += cameraPanFactor;
-            mainCameraTarget.transform.rotation = Quaternion.Euler(mainCameraRotation);
-        }
-        
         // Fly over the board in all directions
-        if (Input.GetKey(KeyCode.W)) cameraContainerMovement += new Vector3(0, 0, 1);
-        if (Input.GetKey(KeyCode.A)) cameraContainerMovement += new Vector3(-1, 0, 0);
-        if (Input.GetKey(KeyCode.S)) cameraContainerMovement += new Vector3(0, 0, -1);        
-        if (Input.GetKey(KeyCode.D)) cameraContainerMovement += new Vector3(1, 0, 0);
-        
-        // Rotate Camera with Q & E
-        if (Input.GetKey(KeyCode.E)) cameraContainerTarget.transform.RotateAround(cameraHitPoint.transform.position, Vector3.up, 100f * Time.deltaTime);
-        if (Input.GetKey(KeyCode.Q)) cameraContainerTarget.transform.RotateAround(cameraHitPoint.transform.position, Vector3.up, -100f * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W)) cameraTranslation += cameraOffsetTarget.transform.forward;
+        if (Input.GetKey(KeyCode.D)) cameraTranslation += cameraOffsetTarget.transform.right;
+        if (Input.GetKey(KeyCode.S)) cameraTranslation += cameraOffsetTarget.transform.forward * -1;
+        if (Input.GetKey(KeyCode.A)) cameraTranslation += cameraOffsetTarget.transform.right * -1;
 
-        // Add movement 
-        cameraContainerTarget.transform.Translate(cameraContainerMovement * speed * Time.deltaTime);
+        // Rotate Camera with Q & E
+        if (Input.GetKey(KeyCode.E)) targetRotation *= Quaternion.Euler(0, 1, 0);
+        if (Input.GetKey(KeyCode.Q)) targetRotation *= Quaternion.Euler(0, -1, 0);
+
+        // Add translation to position
+        cameraTranslation.Normalize();
+        targetPosition += cameraTranslation * speed * Time.deltaTime;
+
+        // Calculate absolute offset based on zoom step
+        targetOffset = new Vector3(0,
+            zoomOffsetYStep * currentStep,
+            zoomOffsetZStep * currentStep);
+
+        // Calculate absolute camera tilt based on zoom step
+        targetTilt = Quaternion.Euler(zoomTiltStep * currentStep, 0, 0);
+
+        // Set the target objects
+        cameraCenterTarget.transform.localPosition = targetPosition;
+        cameraCenterTarget.transform.localRotation = targetRotation;
+        cameraOffsetTarget.transform.localPosition = targetOffset;
+        mainCameraTarget.transform.localRotation = targetTilt;
+
+        // Get a position above the actual camera position
+        Vector3 mainCameraRaycastStart = new Vector3(
+            mainCameraTarget.transform.position.x,
+            500,
+            mainCameraTarget.transform.position.z);
+
+        // Shoot ray down to terrain
+        if (Physics.Raycast(
+                mainCameraRaycastStart,
+                Vector3.down,
+                out hit, 1000, LayerMask.GetMask(
+                                LayerMask.LayerToName(Constants.worldFloorLayer),
+                                LayerMask.LayerToName(Constants.tileLayer))))
+        {
+            // Check if the camera below the terrain. If so, move up
+            if (hit.point.y > targetPosition.y )
+            {
+                targetPosition.y = hit.point.y;
+                cameraCenterTarget.transform.localPosition = targetPosition;
+                Debug.Log("Adding height");
+            }
+        }
+
+        // Calculate the interpolated positions
+        currentPosition = Vector3.Lerp(currentPosition, targetPosition, Time.deltaTime * cameraPositionLerpSpeed);
+        currentRotation = Quaternion.Lerp(currentRotation, targetRotation , Time.deltaTime * cameraRotationLerpSpeed);
+        currentOffset = Vector3.Lerp(currentOffset, targetOffset, Time.deltaTime * cameraPositionLerpSpeed);
+        currentTilt = Quaternion.Lerp(currentTilt, targetTilt, Time.deltaTime * cameraRotationLerpSpeed);
 
         // Move actual camera
-        mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, mainCameraTarget.transform.rotation, Time.deltaTime * cameraRotationLerpSpeed);
-        cameraContainer.transform.rotation = Quaternion.Lerp(cameraContainer.transform.rotation, cameraContainerTarget.transform.rotation , Time.deltaTime * cameraRotationLerpSpeed);
-        cameraContainer.transform.position = Vector3.Lerp(cameraContainer.transform.position, cameraContainerTarget.transform.position, Time.deltaTime * cameraPositionLerpSpeed);
+        cameraCenter.transform.localPosition = currentPosition;
+        cameraCenter.transform.localRotation = currentRotation;
+        cameraOffset.transform.localPosition = currentOffset;
+        mainCamera.transform.localRotation = currentTilt;
     }
 
     public void SetCameraRotation(Quaternion cameraRotation)    
     {
-        mainCameraTarget.transform.rotation = cameraRotation;
+        targetRotation = cameraRotation;
     }
 
     public Quaternion GetCameraRotation()
     {
-        return mainCameraTarget.transform.rotation;
+        return targetRotation;
     }
 
     public void SetCameraContainerPosition(Vector3 cameraContainerPosition)
     {
-        cameraContainerTarget.transform.position = cameraContainerPosition;
+        targetPosition = cameraContainerPosition;
     }
 
     public Vector3 GetCameraContainerPosition()
     {
-        return cameraContainerTarget.transform.position;
+        return targetPosition;
     }
 
     public void SetCameraContainerRotation(Quaternion cameraContainerRotation)
     {
-        cameraContainerTarget.transform.rotation = cameraContainerRotation;
+        targetRotation = cameraContainerRotation;
     }
 
     public Quaternion GetCameraContainerRotation()
     {
-        return cameraContainerTarget.transform.rotation;
+        return targetRotation;
     }
 
     public void SetZoomLevel(int zoomLevel)
     {
-        currentZoomLevel = zoomLevel;
+        currentStep = zoomLevel;
     }
 
     public int GetZoomLevel()
     {
-        return currentZoomLevel;
+        return currentStep;
     }
 }
